9b8a1aac6b1267bb1609291f6cc6157cf5dee11b
irisgal17 <irisgalgal@outlook.com>
2022-07-31 20:10:26 -0500

    Create FeedItem

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by IrisGal on 29/07/22.
+//
+
+import Foundation
+
+struct FeedItem{
+    let id : UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
