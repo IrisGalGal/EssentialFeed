f09dec96745da6cc2b2b4d6a3c877bd11f8cb8ab
irisgalgal <irisgalgal017@gmail.com>
2022-10-03 12:52:50 -0500

    Load command requests cache retrieval

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -12,6 +12,8 @@ import EssentialFeed
     enum ReceivedMessage: Equatable{
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
+        
     }
     private(set) var receivedMessages = [ReceivedMessage]()
     
@@ -38,4 +40,7 @@ import EssentialFeed
     func completeInsertionSuccessfully(at index: Int = 0){
         insertionCompletions[index](nil)
     }
+     func retrieve() {
+         receivedMessages.append(.retrieve)
+     }
 }
