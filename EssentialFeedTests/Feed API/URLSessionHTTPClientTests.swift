82deb4f291761702bc9dd66d87b7c2ae1d94969a
irisgalgal <irisgalgal017@gmail.com>
2022-10-24 17:04:39 -0500

    Simplify URLSessionHTTPClient completion code with the new Result APIs

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -106,11 +106,11 @@ class URLSessionHTTPClientTests: XCTestCase{
             return nil
         }
     }
-    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line:UInt = #line) -> HTTPClientResult{
+    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line:UInt = #line) -> HTTPClient.Result{
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         let exp = expectation(description: "wait for completion")
-        var receivedResult: HTTPClientResult!
+        var receivedResult: HTTPClient.Result!
         sut.get(from: anyURL()){ result in
             receivedResult = result
             exp.fulfill()
