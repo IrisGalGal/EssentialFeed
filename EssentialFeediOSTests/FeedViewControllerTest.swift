f16b46eea8af824f642d1880a797caf1461d63cd
irisgalgal <irisgalgal017@gmail.com>
2022-10-26 18:42:12 -0500

    Cancel image URL preloading when image view is not near visible anymore

--- EssentialFeediOSTests/FeedViewControllerTest.swift
+++ EssentialFeediOSTests/FeedViewControllerTest.swift
@@ -228,6 +228,22 @@ final class FeedViewControllerTest: XCTestCase {
         }
     }
     
+    func test_feedImageView_cancelsImageURLPreloadingWhenNotNearVisibleAnymore() {
+        let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1])
+        XCTAssertEqual(loader.cancelledImageURLs, [], "Expected no cancelled image URL requests until image is not near visible")
+
+        sut.simulateFeedImageViewNotNearVisible(at: 0)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.url], "Expected first cancelled image URL request once first image is not near visible anymore")
+
+        sut.simulateFeedImageViewNotNearVisible(at: 1)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
+    }
+    
     private func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #file, line: UInt = #line) {
         let view = sut.feedImageView(at: index)
 
@@ -339,6 +355,13 @@ private extension FeedViewController {
         let index = IndexPath(row: row, section: feedImagesSection)
         ds?.tableView(tableView, prefetchRowsAt: [index])
     }
+    func simulateFeedImageViewNotNearVisible(at row: Int) {
+        simulateFeedImageViewNearVisible(at: row)
+
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
  }
 
 private extension FeedImageCell{
@@ -365,6 +388,7 @@ private extension FeedImageCell{
     func simulateRetryAction() {
         feedImageRetryButton.simulateTap()
     }
+    
 }
 
 private extension UIRefreshControl{
