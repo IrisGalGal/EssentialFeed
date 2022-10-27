6e3045c6b1a9b9cd222289bb7ad2b285471c8097
irisgalgal <irisgalgal017@gmail.com>
2022-10-27 13:09:10 -0500

    Format access control order following convections

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import EssentialFeed
-final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching{
+ public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching{
     private var refreshController : FeedRefreshViewController?
     private var tableModel = [FeedImage]() {
         didSet { tableView.reloadData() }
