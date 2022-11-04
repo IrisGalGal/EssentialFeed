b097bc3a6d26e2a36a23c83494146934ed341ad2
irisgalgal <irisgalgal017@gmail.com>
2022-11-04 10:42:46 -0600

    Display image with animation

---
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animatios.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Animatios.swift
+//  EssentialFeediOS
+//
+//  Created by IrisDarka on 04/11/22.
+//
+
+import UIKit
+
+extension UIImageView{
+    func setImageAnimated(_ newImage: UIImage?){
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
