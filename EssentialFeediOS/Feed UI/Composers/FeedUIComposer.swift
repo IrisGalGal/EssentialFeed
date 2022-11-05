b6cbc76ff286abe2ae99130cff1f3be4b0e00aee
irisgalgal <irisgalgal017@gmail.com>
2022-11-05 17:03:16 -0600

    move MainqueueDisptachDecorator to his own file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -33,33 +33,8 @@ private extension FeedViewController{
         return feedController
     }
 }
-private final class MainqueueDisptachDecorator<T>{
-    private let decorator: T
-    
-    init(decorator: T) {
-        self.decorator = decorator
-    }
-    func dispatch(completion: @escaping () -> Void){
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        completion()
-    }
-}
-extension MainqueueDisptachDecorator: FeedLoader where T == FeedLoader{
-    func load(completion: @escaping (LoadFeedResult) -> Void) {
-        decorator.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-extension MainqueueDisptachDecorator: FeedImageDataLoader where T == FeedImageDataLoader{
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        decorator.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
+
+
 private final class WeakRefVirtualProxy <T: AnyObject>{
     private weak var object: T?
     
