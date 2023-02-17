6a43b8a393c3770bde9206f5dc68a6206ac961af
irisgalgal <irisgalgal017@gmail.com>
2023-02-17 11:51:43 -0600

    Move RemoteFeedImageDataLoader to production

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -7,10 +7,58 @@
 
 import Foundation
 
-//public final class RemoteFeedImageDataLoader: FeedImageDataLoader{
-//    public func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoader {
-//        <#code#>
-//    }
-//    
-//    
-//}
+class RemoteFeedImageDataLoader{
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient){
+        self.client = client
+    }
+    
+    public enum Error: Swift.Error{
+        case invalidData
+    }
+    
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result){
+            completion?(result)
+        }
+        
+        func cancel() {
+            self.preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions(){
+            completion = nil
+        }
+    }
+    
+    @discardableResult
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        var task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success(data, response):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with: .success(data))
+                } else {
+                    task.complete(with: .failure(Error.invalidData))
+                }
+            case let .failure(error): task.complete(with: .failure(error))
+            }
+        }
+        return task
+    }
+}
