265037573979dd9b0bf9506a4e58dc255311db6b
irisgalgal <irisgalgal017@gmail.com>
2023-03-01 21:34:09 -0600

    Move CoreDataFeedStore implementation of the FeedStore protocol to separate file

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by IrisGal on 19/10/22.
+//
+
+import CoreData
+
+public final class CoreDataFeedStore {
+    private let container : NSPersistentContainer
+    private let context : NSManagedObjectContext
+    
+    public init(storeURL: URL,bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+            let context = self.context
+            context.perform { action(context) }
+        }
+}
