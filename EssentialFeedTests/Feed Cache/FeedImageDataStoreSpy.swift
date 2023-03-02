a359fac1a9c80a2bd6bd73b59203903507b088e0
irisgalgal <irisgalgal017@gmail.com>
2023-03-01 18:33:58 -0600

    LocalFeedImageDataLoader.saveImageDataForURL requests image data insertion into store

--- EssentialFeedTests/Feed Cache/FeedImageDataStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/FeedImageDataStoreSpy.swift
@@ -8,20 +8,22 @@
 import Foundation
 import EssentialFeed
 
-public class FeedImageDataStoreSpy: FeedImageDataStore {
+class FeedImageDataStoreSpy: FeedImageDataStore {
     enum Message: Equatable {
         case insert(data: Data, for: URL)
         case retrieve(dataFor: URL)
     }
-
+    
     private(set) var receivedMessages = [Message]()
     private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
 
-    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
     
-    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         receivedMessages.append(.retrieve(dataFor: url))
         retrievalCompletions.append(completion)
     }
@@ -32,5 +34,9 @@ public class FeedImageDataStoreSpy: FeedImageDataStore {
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
