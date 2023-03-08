3c9e3ceba10fcc7309207cc5d9caaa2fffc5d3b3
irisgalgal <irisgalgal017@gmail.com>
2023-03-08 17:22:40 -0600

    FeedLoaderWithFallbackComposite.load delivers primary feed on primary loader success

---
+++ EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,54 @@
+//
+//  RemoteWithLocalFallbackFeedLoaderTests.swift
+//  EssentialAppTests
+//
+//  Created by IrisDarka on 08/03/23.
+//
+
+import XCTest
+import EssentialFeed
+
+
+class FeedLoaderWithFallbackComposite: FeedLoader{
+    private let primary : FeedLoader
+    init(primary: FeedLoader, fallback: FeedLoader){
+        self.primary = primary
+        
+    }
+    func load(completion: @escaping (LoadFeedResult) -> Void) {
+        primary.load(completion: completion)
+    }
+}
+class FeedLoaderWithFallbackCompositeTests: XCTestCase{
+    func test_load_deliversRemoteFeedOnRemoteSuccess(){
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        let primaryLoader = LoaderStub(result: .success(primaryFeed))
+        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result{
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, primaryFeed)
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    private func uniqueFeed() -> [FeedImage]{
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+    }
+    private class LoaderStub: FeedLoader{
+        private let result: FeedLoader.LoadFeedResult
+        init(result: FeedLoader.LoadFeedResult) {
+            self.result = result
+        }
+        func load(completion: @escaping (LoadFeedResult) -> Void) {
+            completion(result)
+        }
+    }
+}
