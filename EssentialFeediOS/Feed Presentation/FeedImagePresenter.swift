2224707657626125750b67c294e580fc971613f3
Iris GalGal <irisgalgal017@gmail.com>
2023-03-17 12:56:57 -0600

    fix issue not showing data so updating data in FeedImagePresenter. Passing to it own file FeedViewModel Passing to it own file FeedLoadingViewModel

--- EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
@@ -21,13 +21,32 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
         self.imageTransformer = imageTransformer
     }
     func didStartLoadingImageData(for model: FeedImage){
-    
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shoucldRetry: false))
     }
     private struct InvalidImageDataError: Error{}
     func didFinishLoadingImageData(with data: Data, for model: FeedImage){
-        
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: image,
+            isLoading: false,
+            shouldRetry: false))
     }
     func didFinishLoadingImageData(with error: Error, for model: FeedImage){
-        
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true))
     }
 }
---
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Iris GalGal on 17/03/23.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel{
+    let feed: [FeedImage]
+}
