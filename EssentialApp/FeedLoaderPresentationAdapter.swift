7ea62da4b092603372039b88f3333cd349f3692f
Iris GalGal <irisgalgal017@gmail.com>
2023-03-27 15:10:55 -0600

    Move dispatchOnMainQueue operator to the point of use (before subsiription with `sink`)

--- EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -4,7 +4,7 @@
 //
 //  Created by IrisDarka on 05/11/22.
 //
-import UIKit
+
 import Combine
 import EssentialFeed
 import EssentialFeediOS
@@ -21,16 +21,18 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
     func didRequestFeedRefresh() {
         presenter?.didStartLoadingFeed()
         
-        cancellable = feedLoader().sink(
-            receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .finished: break
-                    
-                case let .failure(error):
-                    self?.presenter?.didFinishLoadingFeed(with: error)
-                }
-            }, receiveValue: { [weak self] feed in
-                self?.presenter?.didFinishLoadingFeed(with: feed)
-            })
+        cancellable = feedLoader()
+            .dispatchOnMainQueue()
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                        
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoadingFeed(with: error)
+                    }
+                }, receiveValue: { [weak self] feed in
+                    self?.presenter?.didFinishLoadingFeed(with: feed)
+                })
     }
 }
