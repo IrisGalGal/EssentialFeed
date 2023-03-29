f4ad9312fa7d007a5c8f2033d179b67667f20abd
Iris GalGal <irisgalgal017@gmail.com>
2023-03-28 18:10:23 -0600

    Move FeedImage mapping to the FeedItemsMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -40,8 +40,3 @@ public class RemoteFeedLoader: FeedLoader{
         }
     }
 }
-private extension Array where Element == RemoteFeedItem{
-    func toModels() -> [FeedImage]{
-        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image)}
-    }
-}
