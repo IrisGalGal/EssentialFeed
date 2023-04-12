76e12f4b1c38791c8702bcaf8bfc71329c80b672
Iris GalGal <irisgalgal017@gmail.com>
2023-04-11 18:43:30 -0600

    Replace FeedPresenter with generic presenter

--- EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -8,10 +8,10 @@
 import Foundation
 public protocol ResourceView{
     associatedtype ResourceViewModel
-    func display(_ viewModel: String)
+    func display(_ viewModel: ResourceViewModel)
 }
 public final class LoadResourcePresenter<Resource, View: ResourceView>{
-    public typealias Mapper = (Resource) -> String
+    public typealias Mapper = (Resource) throws -> View.ResourceViewModel
 
     private let resourceView: View
     private let loadingView: ResourceLoadingView
@@ -24,6 +24,12 @@ public final class LoadResourcePresenter<Resource, View: ResourceView>{
         self.errorView = errorView
         self.mapper = mapper
     }
+    public init(resourceView: View, loadingView: ResourceLoadingView, errorView: ResourceErrorView) where Resource == View.ResourceViewModel {
+        self.resourceView = resourceView
+        self.loadingView = loadingView
+        self.errorView = errorView
+        self.mapper = { $0 }
+    }
     
     public static var loadError: String{
         return NSLocalizedString("GENERIC_CONNECTION_ERROR",
@@ -32,18 +38,22 @@ public final class LoadResourcePresenter<Resource, View: ResourceView>{
                                  comment: "Error message displayed when we can't resource the image feed from the server")
     }
     public func didStartLoading() {
-        errorView.display(ResourceErrorViewModel(message: nil))
+        errorView.display(.noError)
         loadingView.display(ResourceLoadingViewModel(isLoading: true))
     }
 
     public func didFinishLoading(with resource: Resource) {
-        resourceView.display(mapper(resource))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        do {
+            resourceView.display(try mapper(resource))
+            loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        } catch {
+            didFinishLoading(with: error)
+        }
     }
 
     public func didFinishLoading(with error: Error) {
+        errorView.display(.error(message: Self.loadError))
         loadingView.display(ResourceLoadingViewModel(isLoading: false))
-        errorView.display(ResourceErrorViewModel(message: FeedPresenter.errorTitle))
     }
 
 }
