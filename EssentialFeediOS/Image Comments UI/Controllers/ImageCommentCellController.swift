a2571376ed4581e6b71caea5a6fe562fcac9853e
Iris GalGal <irisgalgal017@gmail.com>
2023-04-19 19:35:11 -0600

    Implementing image comment test

---
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Iris GalGal on 19/04/23.
+//
+
+import UIKit
+import EssentialFeed
+
+public class ImageCommentCellController: CellController{
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel){
+        self.model = model
+    }
+    public func view(in tableView: UITableView) -> UITableViewCell{
+        let cell : ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+    public func preload(){
+        
+    }
+    public func cancelLoad(){
+        
+    }
+}
