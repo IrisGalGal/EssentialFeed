2e48194f1b1e0f8618370ca3749b46ab438cc160
Iris GalGal <irisgalgal017@gmail.com>
2023-05-17 23:17:19 -0600

    making chances of paginated and removing clases that we no need anymore

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -8,15 +8,17 @@
 import UIKit
 import EssentialFeed
 import EssentialFeediOS
-final class FeedViewAdapter: ResourceView{
-    private weak var controller : ListViewController?
+
+final class FeedViewAdapter: ResourceView {
+    private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private let selection: (FeedImage) -> Void
+    
     private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
     
     private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
-
-    private let selection: (FeedImage) -> Void
-    init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher, selection: @escaping (FeedImage) -> Void = { _ in }) {
+    
+    init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher, selection: @escaping (FeedImage) -> Void) {
         self.controller = controller
         self.imageLoader = imageLoader
         self.selection = selection
@@ -24,16 +26,15 @@ final class FeedViewAdapter: ResourceView{
     
     func display(_ viewModel: Paginated<FeedImage>) {
         let feed: [CellController] = viewModel.items.map { model in
-            
-            let adapter = ImageDataPresentationAdapter(loader: {[imageLoader] in
+            let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
                 imageLoader(model.url)
             })
             
             let view = FeedImageCellController(
-                viewModel:  FeedImagePresenter.map(model),
+                viewModel: FeedImagePresenter.map(model),
                 delegate: adapter,
                 selection: { [selection] in
-                    self.selection(model)
+                    selection(model)
                 })
             
             adapter.presenter = LoadResourcePresenter(
@@ -44,24 +45,27 @@ final class FeedViewAdapter: ResourceView{
             
             return CellController(id: model, view)
         }
-            guard let loadMorePublisher = viewModel.loadMorePublisher else {
-                controller?.display(feed)
-                return
-            }
-            let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
-            let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
         
-            loadMoreAdapter.presenter = LoadResourcePresenter(
+        guard let loadMorePublisher = viewModel.loadMorePublisher else {
+            controller?.display(feed)
+            return
+        }
+        
+        let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
+        
+        let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
+        
+        loadMoreAdapter.presenter = LoadResourcePresenter(
             resourceView: self,
             loadingView: WeakRefVirtualProxy(loadMore),
-            errorView: WeakRefVirtualProxy(loadMore),
-            mapper: { $0 })
-            
-            let loadMoreSection = [CellController(id: UUID(), loadMore)]
+            errorView: WeakRefVirtualProxy(loadMore))
         
-            controller?.display(feed, loadMoreSection)
-        }
+        let loadMoreSection = [CellController(id: UUID(), loadMore)]
+        
+        controller?.display(feed, loadMoreSection)
+    }
 }
+
 extension UIImage {
     struct InvalidImageData: Error {}
     
--- EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -11,50 +11,98 @@ import EssentialFeediOS
 extension ListViewController {
     public override func loadViewIfNeeded() {
         super.loadViewIfNeeded()
+        
         tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
     }
+    
     func simulateUserInitiatedReload() {
         refreshControl?.simulatePullToRefresh()
     }
+    
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
     }
     
-    func simulateErrorViewTap(){
+    func simulateErrorViewTap() {
         errorView.simulateTap()
     }
-    var errorMessage: String?{
+    
+    var errorMessage: String? {
         return errorView.message
     }
-}
-extension ListViewController {
     
-    func numberOfRenderedComments() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
-    }
-    private var commentsSection: Int {
-        return 0
+    func numberOfRows(in section: Int) -> Int {
+        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
     }
     
-    func commentMessage(at row: Int) -> String?{
-        commentView(at: row)?.messageLabel.text
-    }
-    func commentDate(at row: Int) -> String?{
-        commentView(at: row)?.dateLabel.text
-    }
-    func commentUsername(at row: Int) -> String?{
-        commentView(at: row)?.usernameLabel.text
-    }
-    private func commentView(at row: Int) -> ImageCommentCell? {
-        guard numberOfRenderedComments() > row else{
+    func cell(row: Int, section: Int) -> UITableViewCell? {
+        guard numberOfRows(in: section) > row else {
             return nil
         }
         let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: commentsSection)
-        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+        let index = IndexPath(row: row, section: section)
+        return ds?.tableView(tableView, cellForRowAt: index)
     }
 }
+
 extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        numberOfRows(in: commentsSection)
+    }
+    
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        cell(row: row, section: commentsSection) as? ImageCommentCell
+    }
+    
+    private var commentsSection: Int { 0 }
+}
+
+extension ListViewController {
+    
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+        return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    @discardableResult
+    func simulateFeedImageBecomingVisibleAgain(at row: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewNotVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, willDisplay: view!, forRowAt: index)
+        
+        return view
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+        
+        return view
+    }
+    
+    func simulateTapOnFeedImage(at row: Int) {
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didSelectRowAt: index)
+    }
     
     func simulateFeedImageViewNearVisible(at row: Int) {
         let ds = tableView.prefetchDataSource
@@ -70,64 +118,48 @@ extension ListViewController {
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
     
-  
-    @discardableResult
-    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-        return feedImageView(at: index) as? FeedImageCell
+    func simulateLoadMoreFeedAction() {
+        guard let view = loadMoreFeedCell() else { return }
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
     }
     
-    @discardableResult
-    func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
-        let view = simulateFeedImageViewVisible(at: row)
-        
+    func simulateTapOnLoadMoreFeedError() {
         let delegate = tableView.delegate
-        let index = IndexPath(row: row, section: feedImagesSection)
-        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-        
-        return view
-    }
-   
-    func simulateTapOnFeedImage(at row: Int){
-        let delegate = tableView.delegate
-        let index = IndexPath(row: row, section: feedImagesSection)
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
         delegate?.tableView?(tableView, didSelectRowAt: index)
     }
     
-    func numberOfRenderedFeedImageViews() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
+    var isShowingLoadMoreFeedIndicator: Bool {
+        return loadMoreFeedCell()?.isLoading == true
     }
     
-    func feedImageView(at row: Int) -> UITableViewCell? {
-        guard numberOfRenderedFeedImageViews() > row else {
-            return nil
-        }
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        return ds?.tableView(tableView, cellForRowAt: index)
+    var loadMoreFeedErrorMessage: String? {
+        return loadMoreFeedCell()?.message
+    }
+    
+    var canLoadMoreFeed: Bool {
+        loadMoreFeedCell() != nil
+    }
+    
+    private func loadMoreFeedCell() -> LoadMoreCell? {
+        cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
     
     func renderedFeedImageData(at index: Int) -> Data? {
         return simulateFeedImageViewVisible(at: index)?.renderedImage
     }
     
-    func simulateLoadMoreFeedAction(){
-        guard let view = cell(row: 0, section: feedLoadMoreSection) else {return}
-        
-        let delegate = tableView.delegate
-        let index = IndexPath(row: 0, section: feedLoadMoreSection)
-        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    func numberOfRenderedFeedImageViews() -> Int {
+        numberOfRows(in: feedImagesSection)
     }
-    func cell(row: Int, section: Int) -> UITableViewCell?{
-        guard numberOfRows(in: section) > row else{
-            return nil
-        }
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: section)
-        return ds?.tableView(tableView, cellForRowAt: index)
+    
+    func feedImageView(at row: Int) -> UITableViewCell? {
+        cell(row: row, section: feedImagesSection)
     }
-    func numberOfRows(in section: Int) -> Int {
-        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
-    }
+    
     private var feedImagesSection: Int { 0 }
     private var feedLoadMoreSection: Int { 1 }
 }
