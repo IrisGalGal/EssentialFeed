5a3489c4d69a49604f4db86ee1f96ee844ea9ad3
Iris GalGal <irisgalgal017@gmail.com>
2023-05-22 10:33:19 -0600

    Make CoreData FeedStore implementation sync

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -7,48 +7,35 @@
 
 import Foundation
 
-protocol FeedStoreSpecs{
-     func test_retrieve_deliversEmptyOnEmptyCache()
-
-     func test_retrieve_hasNoSideEffects()
-
-     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-
-     func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+protocol FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnEmptyCache()
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
     
-
-     func test_insert_deliversNoErrorOnEmptyCache()
-
-     func test_insert_deliversNoErrorNonEmptyCache()
-
-     func test_insert_overridesPreviuslyInsertedCacheValues()
-
-
-     func test_delete_deliversNoErrorOnEmptyCache()
-
-     func test_delete_hasNoSideEffectsOnEmptyCache()
-
-     func test_delete_deliversNoErrorOnNonEmptyCache()
-
-     func test_delete_emptiesPreviouslyInsertedCache()
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
+    func test_insert_overridesPreviouslyInsertedCacheValues()
     
-
-     func test_storeSideEffects_runSerially()
+    func test_delete_deliversNoErrorOnEmptyCache()
+    func test_delete_hasNoSideEffectsOnEmptyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
+    func test_delete_emptiesPreviouslyInsertedCache()
 }
 
-protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs{
-    func test_retrieve_deliversFailureOnRetrieveError()
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+    func test_retrieve_deliversFailureOnRetrievalError()
     func test_retrieve_hasNoSideEffectsOnFailure()
 }
 
-protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs{
-    func test_insert_deliversErrorOnInsertioError()
-    func test_insert_haNoSideEffectsOnInsertioError()
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
+    func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectsOnInsertionError()
 }
 
-protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs{
+protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
     func test_delete_deliversErrorOnDeletionError()
     func test_delete_hasNoSideEffectsOnDeletionError()
 }
 
-typealias FailableFeedStore = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
+typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
